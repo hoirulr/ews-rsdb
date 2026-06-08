d7d409562026df709ed901c186f834c4f22787c6
Hoirul Rijal <hoirulr@gmail.com>
2026-06-09 06:42:00 +0700

    perbaikan loading terus

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -20,7 +20,7 @@ new #[Layout('layouts.guest')] class extends Component
 
         Session::regenerate();
 
-        $this->redirect(route('dashboard', absolute: false), navigate: true);
+        $this->redirect(route('dashboard', absolute: false));
     }
 }; ?>
 
